8478a106782ab205132b4b3d60311f80b9370c08
mhazizk <mhazizk@gmail.com>
2022-11-06 00:49:02 +0700

    update 0.3.5

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,6 @@ import axios from 'axios'
 import PokemonDetailsPage from './PokemonDetails'
 import pokeball from '../assets/pokeball.png'
 
-
 const Home = () => {
 
     // state to save searchQuery
